33cef815e7e15cb2bb6f97b1a6ec426c937020af
Denis Cherny <denis.chernyy888@gmail.com>
2022-05-18 23:13:36 +0300

    Create kmm project

---
+++ Todoshka/settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Todoshka"
+include(":androidApp")
+include(":shared")
---
+++ Todoshka/shared/src/commonMain/kotlin/com/example/todoshka/Platform.kt
@@ -0,0 +1,5 @@
+package com.example.todoshka
+
+expect class Platform() {
+    val platform: String
+}
